7b111635a16b4f8fac947d3d892e52d08f99691c
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-06-26 18:02:53 +0700

    bo sung bai 1

--- src/main/java/controller/Counter3Controller.java
+++ src/main/java/controller/Counter3Controller.java
@@ -1,35 +1,22 @@
 package controller;
 
-
 import model.Counter;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.function.RequestPredicates;
-import org.springframework.web.servlet.function.RouterFunctions;
 
 @Controller
-@SessionAttributes("visitor")
-public class CounterController {
-
+public class Counter3Controller {
 
     @ModelAttribute("visitor")
     public Counter setCounter(){
         return new Counter();
     }
 
-    @GetMapping("/index")
-    public String get(@ModelAttribute("visitor") Counter counter){
-        counter.increment();
-        return "/index";
-    }
-
-    @GetMapping("/index2")
+    @GetMapping("/index3")
     public String get2(@SessionAttribute("visitor") Counter counter){
         counter.increment();
-        return "/index2";
+        return "/index3";
     }
 }
