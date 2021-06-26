ccfe25fc8649ffce86a32b17703ce83587cd5318
hoang-gtvt <hoang92.gtvt@gmail.com>
2021-06-26 18:22:50 +0700

    bo sung bai 1 lan 2

--- src/main/java/controller/Counter3Controller.java
+++ src/main/java/controller/Counter3Controller.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+
 public class Counter3Controller {
 
     @ModelAttribute("visitor")
